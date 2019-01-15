fd15024184f2be833c265a8bf42f9087ee016a0e
Ido Berkovich <idoberko2@gmail.com>
2019-01-15 22:08:28 +0200

    Fixed watermark

--- utils/processImage.js
+++ utils/processImage.js
@@ -2,22 +2,22 @@ const fs = require('fs');
 const sizeOf = require('image-size');
 const sharp = require('sharp');
 
-const logo = fs.readFileSync(`${__dirname}/../assets/Hapoel_Katamon_FC.png`);
-const resizeTo = process.env.RESIZE_PIXELS || 1000;
+const logo = fs.readFileSync(`${__dirname}/../assets/watermark.png`);
+const resizeTo = process.env.RESIZE_PIXELS || 900;
 
 function processImage(image) {
     const {
         width,
         height,
     } = sizeOf(image);
-    
+
     const isLandscape = width > height;
     const resizeWidthTo = isLandscape ? resizeTo : null;
     const resizeHeightTo = isLandscape ? null : resizeTo;
 
     return sharp(image)
         .overlayWith(logo, {
-            gravity: sharp.gravity.southeast,
+            gravity: sharp.gravity.northwest,
         })
         .resize(resizeWidthTo, resizeHeightTo);
 }
