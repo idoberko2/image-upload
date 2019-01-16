a049870dee26033169ab5d21a1c235dfbb07ecee
Ido Berkovich <idoberko2@gmail.com>
2019-01-16 22:02:05 +0200

    Client-side functionality - WIP

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const uploader = document.getElementById('uploader');
-    uploader.addEventListener('change', function () {
-        const promises = Array
-            .from(this.files)
-            .map(file => {
+function sendFiles(collection, files) {
+    const promises = files.map(file => {
                 const formData = new FormData();
 
                 formData.append('image', file);
-                // TODO: take value from form
-                formData.append('collection', 'katamon vs kfar saba');
+                formData.append('collection', collection);
 
                 axios.post('/upload', formData, {
                     headers: {
@@ -17,9 +12,51 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
             });
 
-        axios
+        return axios
             .all(promises)
             .then(() => console.log('mush success!'))
             .error(err => console.error({err}));
+}
+
+function validateFiles(files) {
+    for (file of files) {
+        if (!/image\/[a-zA-Z]+$/.test(file.type)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let files;
+    let collection;
+    let isValid = false;
+
+    document.getElementById('collection').addEventListener('keyup', function (event) {
+        collection = event.target.value;
+    });
+
+    const uploader = document.getElementById('uploader');
+    uploader.addEventListener('change', function () {
+        const filesArray = Array.from(this.files);
+        if (window.FileReader && window.Blob && validateFiles(filesArray)) {
+            files = filesArray;
+            document.getElementById('status').innerHTML = '✅ ' + files.length + ' קבצים נבחרו';
+        } else if (!window.FileReader || !window.Blob) {
+            // can't validate, invalid files might be sent to server
+            files = filesArray;
+        } else {
+            document.getElementById('status').innerHTML = '❌ לפחות אחד מהקבצים אינו תמונה';
+        }
     }, false);
+
+    document.getElementById('submit').addEventListener('click', function () {
+        sendFiles(collection, files);
+    });
+
+    document.getElementById('clear').addEventListener('click', function () {
+        document.getElementById('status').innerHTML = '';
+        uploader.value = null;
+    });
 });
