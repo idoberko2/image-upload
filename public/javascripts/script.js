36decd165dba8d50dbd3792e0411bce72bfbfd66
Ido Berkovich <idoberko2@gmail.com>
2019-01-17 08:54:45 +0200

    Migrate client side to react

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,23 @@
-function sendFiles(collection, files) {
-    const promises = files.map(file => {
-                const formData = new FormData();
+const e = React.createElement;
 
-                formData.append('image', file);
-                formData.append('collection', collection);
+function sendFiles({ collection, files }) {
+    const promises = Array.from(files).map(file => {
+        const formData = new FormData();
 
-                axios.post('/upload', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                });
-            });
+        formData.append('image', file);
+        formData.append('collection', collection);
 
-        return axios
-            .all(promises)
-            .then(() => console.log('mush success!'))
-            .error(err => console.error({err}));
+        axios.post('/upload', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+    });
+
+    return axios
+        .all(promises)
+        .then(() => console.log('mush success!'))
+        .catch(err => console.error({err}));
 }
 
 function validateFiles(files) {
@@ -28,35 +30,145 @@ function validateFiles(files) {
     return true;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    let files;
-    let collection;
-    let isValid = false;
-
-    document.getElementById('collection').addEventListener('keyup', function (event) {
-        collection = event.target.value;
-    });
-
-    const uploader = document.getElementById('uploader');
-    uploader.addEventListener('change', function () {
-        const filesArray = Array.from(this.files);
-        if (window.FileReader && window.Blob && validateFiles(filesArray)) {
-            files = filesArray;
-            document.getElementById('status').innerHTML = '✅ ' + files.length + ' קבצים נבחרו';
-        } else if (!window.FileReader || !window.Blob) {
-            // can't validate, invalid files might be sent to server
-            files = filesArray;
-        } else {
-            document.getElementById('status').innerHTML = '❌ לפחות אחד מהקבצים אינו תמונה';
-        }
-    }, false);
-
-    document.getElementById('submit').addEventListener('click', function () {
-        sendFiles(collection, files);
-    });
-
-    document.getElementById('clear').addEventListener('click', function () {
-        document.getElementById('status').innerHTML = '';
-        uploader.value = null;
-    });
+const UploadStep = React.createClass({
+    render() {
+        return e(
+            'div', 
+            { className: 'upload-steps--item' }, 
+            e('h1', { className: 'upload-steps--item--header' }, `${this.props.step}.`),
+            this.props.children
+        );
+    }
 });
+
+const UploadForm = React.createClass({
+    getInitialState() {
+        return {
+            files: null,
+            collection: '',
+            collectionChanged: false,
+        };
+    },
+
+    handleCollectionChange(event) {
+        const collection = event.target.value;
+        this.setState(prevState => ({
+            collection,
+            collectionChanged: prevState.collectionChanged || Boolean(collection),
+        }));
+    },
+
+    handleFilesSelection(event) {
+        this.setState({
+            files: event.target.files,
+        });
+    },
+
+    isCollectionValid() {
+        return !this.state.collectionChanged || this.state.collection !== '';
+    },
+
+    getCollectionStatus() {
+        return this.isCollectionValid() ? 
+            null :
+            '❌ שם האוסף לא יכול להיות ריק' ;
+    },
+
+    isFilesValid() {
+        return !window.FileReader ||
+            !window.Blob ||
+            (this.state.files && validateFiles(this.state.files));
+    },
+
+    getFilesStatus() {
+        if (!this.state.files) {
+            return null;
+        }
+
+        if (this.isFilesValid()) {
+            return '✅ ' + this.state.files.length + ' קבצים נבחרו';
+        }
+
+        return '❌ לפחות אחד מהקבצים אינו תמונה';
+    },
+
+    isValid() {
+        return this.isCollectionValid() && this.isFilesValid();
+    },
+
+    handleSubmit() {
+        sendFiles(this.state);
+    },
+
+    handleClear() {
+        this.setState({
+            files: null,
+        });
+    },
+
+    render() {
+        return e(
+            'div',
+            { className: 'upload-steps' }, 
+            e(
+                UploadStep, 
+                {step: 1}, 
+                e(
+                    'label',
+                    { htmlFor: 'collection', className: 'text-label' },
+                    'בחירת שם',
+                    e(
+                        'input',
+                        {
+                            type: 'text',
+                            id: 'collection',
+                            className: 'text-input',
+                            placeholder: 'שם האוסף',
+                            value: this.state.collection,
+                            onChange: this.handleCollectionChange,
+                        }
+                    )
+                ),
+                e('div', { className: 'status' }, this.getCollectionStatus())
+            ),
+            e(
+                UploadStep, 
+                {step: 2}, 
+                e(
+                    'label',
+                    { htmlFor: 'uploader', className: 'button uploader-wrapper' },
+                    'בחירת קבצים',
+                    e(
+                        'input',
+                        {
+                            type: 'file',
+                            id: 'uploader',
+                            className: 'uploader-input',
+                            multiple: true,
+                            onChange: this.handleFilesSelection,
+                        }
+                    )
+                ),
+                e('div', { className: 'status' }, this.getFilesStatus())
+            ),
+            e(
+                UploadStep, 
+                {step: 3}, 
+                e(
+                    'div',
+                    { className: 'submit-wrapper' },
+                    e('button',{ 
+                        className: 'button', 
+                        disabled: !this.isValid(),
+                        onClick: this.handleSubmit,
+                    }, 'שליחה'),
+                )
+            )
+        );
+    }
+});
+
+ReactDOM.render(
+    e(UploadForm),
+    document.getElementById('app')
+);
