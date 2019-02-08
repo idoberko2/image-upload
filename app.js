5053e2751080abf2a52b01ff841e1fb86caf2b2a
Ido Berkovich <idoberko2@gmail.com>
2019-02-08 17:59:15 +0200

    Migrated to htm entirely

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/assets/htm', express.static(path.join(__dirname, 'node_modules', 'htm')));
+app.use('/assets/react', express.static(path.join(__dirname, 'node_modules', 'react')));
+app.use('/assets/react-dom', express.static(path.join(__dirname, 'node_modules', 'react-dom')));
 
 app.use('/', indexRouter);
 app.use('/upload', uploadRouter);
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,3 @@
-const e = React.createElement;
 import htm from '/assets/htm/dist/htm.mjs';
 const html = htm.bind(React.createElement);
 const { CSSTransition } = ReactTransitionGroup;
@@ -62,20 +61,33 @@ const UploadStep = ({step, action, children}) => html`
     <//>
 `;
 
-const UploadForm = React.createClass({
-    filesRef: null,
+const uploadFormInitialState = {
+    files: null,
+    collection: '',
+    collectionChanged: false,
+    formVisible: true,
+    successVisible: false,
+    submitError: null,
+    isLoading: false,
+};
 
-    getInitialState() {
-        return {
-            files: null,
-            collection: '',
-            collectionChanged: false,
-            formVisible: true,
-            successVisible: false,
-            submitError: null,
-            isLoading: false,
-        };
-    },
+class UploadForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.filesRef = null;
+        this.state = uploadFormInitialState;
+
+        this.handleCollectionChange = this.handleCollectionChange.bind(this);
+        this.handleFilesSelection = this.handleFilesSelection.bind(this);
+        this.isCollectionValid = this.isCollectionValid.bind(this);
+        this.getCollectionStatus = this.getCollectionStatus.bind(this);
+        this.isFilesValid = this.isFilesValid.bind(this);
+        this.getFilesStatus = this.getFilesStatus.bind(this);
+        this.isValid = this.isValid.bind(this);
+        this.getSubmissionStatus = this.getSubmissionStatus.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
     handleCollectionChange(event) {
         const collection = event.target.value;
@@ -83,7 +95,7 @@ const UploadForm = React.createClass({
             collection,
             collectionChanged: prevState.collectionChanged || Boolean(collection),
         }));
-    },
+    }
 
     handleFilesSelection(event) {
         const { files } = event.target;
@@ -95,23 +107,28 @@ const UploadForm = React.createClass({
         this.setState({
             files,
         });
-    },
+    }
 
     isCollectionValid() {
         return !this.state.collectionChanged || this.state.collection !== '';
-    },
+    }
 
     getCollectionStatus() {
         return this.isCollectionValid() ? 
             null :
-            e('div', {className: 'row-align'}, e(ErrorMark, {className: 'icon__small'}), e('div', null, 'שם האלבום לא יכול להיות ריק'));
-    },
+            html`
+                <div className="row-align">
+                    <${ErrorMark} className="icon__small" />
+                    <div>שם האלבום לא יכול להיות ריק<//>
+                <//>
+            `;
+    }
 
     isFilesValid() {
         return !window.FileReader ||
             !window.Blob ||
             (this.state.files && validateFiles(this.state.files));
-    },
+    }
 
     getFilesStatus() {
         if (!this.state.files) {
@@ -122,31 +139,50 @@ const UploadForm = React.createClass({
             const text = this.state.files.length > 1 ? 
                 this.state.files.length + ' קבצים נבחרו' : 
                 'קובץ אחד נבחר';
-            return e('div', {className: 'row-align'}, e(CheckMark, {className: 'icon__small'}), e('div', null, text));
+            return html`
+                <div className="row-align">
+                    <${CheckMark} className="icon__small" />
+                    <div>${text}<//>
+                <//>
+            `;
         }
 
-        return e('div', {className: 'row-align'}, e(ErrorMark, {className: 'icon__small'}), e('div', null, 'לפחות אחד מהקבצים אינו תמונה'));
-    },
+        return html`
+            <div className="row-align">
+                <${ErrorMark} className="icon__small" />
+                <div>לפחות אחד מהקבצים אינו תמונה<//>
+            <//>
+        `;
+    }
 
     isValid() {
         return this.isCollectionValid() && this.isFilesValid();
-    },
+    }
 
     getSubmissionStatus() {
         return !this.state.submitError ? null :
-            e('div', {className: 'row-align'}, e(ErrorMark, {className: 'icon__small'}), e('div', null, 'קרתה שגיאה במהלך העלאת הקבציפ :('));
-    },
+            html`<div className="row-align">
+                <${ErrorMark} className="icon__small" />
+                <div>קרתה שגיאה במהלך העלאת הקבצים :(<//>
+            <//>`;
+    }
 
     handleSubmit() {
         this.setState({isLoading: true}, () => {
             sendFiles(this.state)
             .then(() => {
                 this.filesRef.value = '';
-                this.setState(Object.assign(this.getInitialState(), { formVisible: false }));
+                this.setState(
+                    Object.assign(
+                        {}, 
+                        uploadFormInitialState, // to reset the form
+                        { formVisible: false } // to hide the form and show success message
+                    )
+                );
             })
             .catch(err => this.setState({submitError: err, isLoading: false}));
         });
-    },
+    }
 
     render() {
         const {
@@ -155,110 +191,96 @@ const UploadForm = React.createClass({
             isLoading,
         } = this.state;
 
-        return e(
-            'div', 
-            {className: 'app'},
-            e(
-                CSSTransition, 
-                {
-                    in: formVisible,
-                    timeout: 300,
-                    classNames: 'upload-steps',
-                    unmountOnExit: true,
-                    onExited: () => this.setState({successVisible: true}),
-                }, 
-                () => e(
-                    'div',
-                    { className: 'upload-steps main' }, 
-                    e(
-                        UploadStep, 
-                        {step: 1, action: 'בוחרים שם לאלבום'}, 
-                        e(
-                                'input',
-                                {
-                                    type: 'text',
-                                    id: 'collection',
-                                    className: 'text-input',
-                                    placeholder: 'שם האלבום',
-                                    value: this.state.collection,
-                                    onChange: this.handleCollectionChange,
-                                    disabled: isLoading,
-                                }
-                            ),
-                        e('div', { className: 'status' }, this.getCollectionStatus())
-                    ),
-                    e(
-                        UploadStep, 
-                        {step: 2, action: 'בוחרים תמונות'}, 
-                        e(
-                            'label',
-                            { htmlFor: 'uploader', className: `button uploader-wrapper ${isLoading ? 'button__disabled' : null}` },
-                            'בחירת תמונות',
-                            e(
-                                'input',
-                                {
-                                    type: 'file',
-                                    id: 'uploader',
-                                    className: 'uploader-input',
-                                    multiple: true,
-                                    onChange: this.handleFilesSelection,
-                                    ref: c => this.filesRef = c,
-                                    disabled: isLoading,
-                                    accept: 'image/*',
-                                }
-                            )
-                        ),
-                        e('div', { className: 'status' }, this.getFilesStatus())
-                    ),
-                    e(
-                        UploadStep, 
-                        {step: 3, action: 'מעלים'}, 
-                        e(
-                            'div',
-                            { className: 'submit-wrapper' },
-                            e(
-                                'button',
-                                { 
-                                    className: 'button', 
-                                    disabled: !this.isValid() || isLoading,
-                                    onClick: this.handleSubmit,
-                                }, 
-                                isLoading ? e('span', { className: 'spinner' }) : e('span', null, 'העלאה')
-                            ),
-                        ),
-                        e('div', { className: 'status' }, this.getSubmissionStatus())
-                    )
-                )
-            ),
-            e(
-                CSSTransition, 
-                {
-                    in: successVisible,
-                    timeout: 500,
-                    classNames: 'success',
-                    unmountOnExit: true,
-                    onExited: () => this.setState({formVisible: true})
-                }, 
-                () => e(
-                    'div',
-                    { className: 'success main' },
-                    e('div', {className: 'checkmark-wrapper'}, e(CheckMark)),
-                    e('div', {className: 'success-message'}, 'הקבצים נשלחו בהצלחה'),
-                    e(
-                        'div', 
-                        {className: 'restart-wrapper'},
-                        e('button',{ 
-                            className: 'button', 
-                            onClick: () => this.setState({successVisible: false}),
-                        }, 'להתחיל מחדש')
-                    )
-                )
-            )
-        );
+        return html`
+            <div className="app">
+                <${CSSTransition}
+                    in=${formVisible}
+                    timeout=${{enter: 500, exit: 300}}
+                    classNames="fade-in-out"
+                    unmountOnExit
+                    onExited=${() => this.setState({successVisible: true})}
+                >
+                    ${
+                        () =>
+                        html`
+                            <div className="main">
+                                <${UploadStep} step="1" action="בוחרים שם לאלבום">
+                                    <input 
+                                        type="text"
+                                        id="collection"
+                                        className="text-input"
+                                        placeholder="שם האלבום"
+                                        value=${this.state.collection}
+                                        onChange=${this.handleCollectionChange}
+                                        disabled=${isLoading}
+                                    />
+                                    <div className="status">${this.getCollectionStatus()}<//>
+                                <//>
+                                <${UploadStep} step="2" action="בוחרים תמונות">
+                                    <label
+                                    htmlFor="uploader"
+                                    className="${`button uploader-wrapper ${isLoading ? 'button__disabled' : null}` }"
+                                    >
+                                        בחירת תמונות
+                                        <input
+                                            type="file"
+                                            id="uploader"
+                                            className="uploader-input"
+                                            multiple
+                                            onChange=${this.handleFilesSelection}
+                                            ref=${c => this.filesRef = c}
+                                            disabled=${isLoading}
+                                            accept="image/*"
+                                        />
+                                    <//>
+                                    <div className="status">${this.getFilesStatus()}<//>
+                                <//>
+                                <${UploadStep} step="3" action="מעלים">
+                                    <div className="submit-wrapper">
+                                        <button
+                                            className="button"
+                                            disabled=${!this.isValid() || isLoading}
+                                            onClick=${this.handleSubmit}
+                                        >
+                                            <span className="${isLoading ? 'spinner' : null}">${isLoading ? null : 'העלאה'}<//>
+                                        <//>
+                                    <//>
+                                    <div className="status">${this.getSubmissionStatus()}<//>
+                                <//>
+                            <//>
+                        `
+                    }
+                <//>
+                <${CSSTransition}
+                    in=${successVisible}
+                    timeout=${{enter: 500, exit: 300}}
+                    classNames="fade-in-out"
+                    unmountOnExit
+                    onExited=${() => this.setState({formVisible: true})}
+                >
+                    ${
+                        () => html`
+                            <div className="main">
+                                <div className="checkmark-wrapper"><${CheckMark} /><//>
+                                <div className="success-message">הקבצים נשלחו בהצלחה<//>
+                                <div className="restart-wrapper">
+                                    <button
+                                        className="button"
+                                        onClick=${() => this.setState({successVisible: false})}
+                                    >
+                                    להתחיל מחדש
+                                    <//>
+                                <//>
+                            <//>
+                        `
+                    }
+                <//>
+            <//>
+        `;
     }
-});
+}
 
 ReactDOM.render(
-    e(UploadForm),
+    html`<${UploadForm} />`,
     document.getElementById('app')
 );
