d904dfed4be2e635f700a98530db9827e5ccd96a
Ido Berkovich <idoberko2@gmail.com>
2020-12-16 20:47:36 +0200

    Configurable watermark
    Changes:
    This commit allows configuring the uploader to not add a watermark

--- utils/processImage.js
+++ utils/processImage.js
@@ -9,11 +9,16 @@ function processImage(image) {
     const resizeWidthTo = isLandscape ? resizeTo : null;
     const resizeHeightTo = isLandscape ? null : resizeTo;
 
-    return sharp(image)
-        .composite([{
+    let sharpImg = sharp(image);
+
+    if (process.env.SHOULD_ADD_WATERMARK === 'true') {
+        sharpImg = sharpImg.composite([{
             input: `${__dirname}/../assets/watermark.png`,
             gravity: sharp.gravity.northwest,
         }])
+    }
+
+    return sharpImg
         .resize(resizeWidthTo, resizeHeightTo)
         .toBuffer();
 }
